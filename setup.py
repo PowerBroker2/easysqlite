4061c2f69148bd5150113b4c434ce31558d10f4c
PB2 <gitstuff2@gmail.com>
2020-12-16 16:24:41 -0500

    `sqlite3` is a built-in package

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as fh:
 setup(
     name             = 'easysqlite',
     packages         = ['easysqlite'],
-    version          = '0.1.1',
+    version          = '0.1.2',
     description      = 'Wrapper librray for SQLite3',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author           = 'Power_Broker',
     author_email     = 'gitstuff2@gmail.com',
     url              = 'https://github.com/PowerBroker2/easysqlite',
-    download_url     = 'https://github.com/PowerBroker2/easysqlite/archive/0.1.1.tar.gz',
+    download_url     = 'https://github.com/PowerBroker2/easysqlite/archive/0.1.2.tar.gz',
     keywords         = ['SQL', 'sqlite3', 'sqlite'],
     classifiers      = [],
-    install_requires = ['sqlite3']
+    install_requires = []
 )
