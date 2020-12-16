58412b1cb22b9d889b9d37c1968e2d3d52aa1f33
PB2 <gitstuff2@gmail.com>
2020-12-16 16:19:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name             = 'pysql',
-    packages         = ['pysql'],
-    version          = '0.1.0',
+    name             = 'easysqlite',
+    packages         = ['easysqlite'],
+    version          = '0.1.1',
     description      = 'Wrapper librray for SQLite3',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author           = 'Power_Broker',
     author_email     = 'gitstuff2@gmail.com',
-    url              = 'https://github.com/PowerBroker2/pysql',
-    download_url     = 'https://github.com/PowerBroker2/pysql/archive/0.1.0.tar.gz',
+    url              = 'https://github.com/PowerBroker2/easysqlite',
+    download_url     = 'https://github.com/PowerBroker2/easysqlite/archive/0.1.1.tar.gz',
     keywords         = ['SQL', 'sqlite3', 'sqlite'],
     classifiers      = [],
     install_requires = ['sqlite3']
